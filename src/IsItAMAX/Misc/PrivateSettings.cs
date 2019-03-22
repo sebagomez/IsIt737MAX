d14201758abb73dc841c53302f02c50c94c98959
seba gomez <sebagomezcorrea@outlook.com>
2019-03-22 12:55:28 -0300

    Revert "Revert "v1""
    This reverts commit d140a89939c17dac6193e5974c9ee26461bef3ef.

---
+++ src/IsItAMAX/Misc/PrivateSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace IsIt737MAX.Misc
+{
+	internal class PrivateSettings
+	{
+		public string UserToken { get; set; }
+		public string UserSecret { get; set; }
+		public string AppKey { get; set; }
+		public string AppSecret { get; set; }
+
+	}
+}
